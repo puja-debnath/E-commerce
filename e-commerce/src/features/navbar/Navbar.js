415c419b2dfda660514747bc168aa89bf74f664f
unknown <104721799+puja-debnath@users.noreply.github.com>
2023-12-21 22:48:09 +0530

    Home update

--- e-commerce/src/features/navbar/Navbar.js
+++ e-commerce/src/features/navbar/Navbar.js
@@ -12,9 +12,7 @@ const user = {
 const navigation = [
   { name: 'Dashboard', href: '#', current: true },
   { name: 'Team', href: '#', current: false },
-  { name: 'Projects', href: '#', current: false },
-  { name: 'Calendar', href: '#', current: false },
-  { name: 'Reports', href: '#', current: false },
+ 
 ]
 const userNavigation = [
   { name: 'Your Profile', href: '#' },
@@ -68,9 +66,14 @@ function Navbar({children}) {
                         className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                       >
                         <span className="absolute -inset-1.5" />
-                        <span className="sr-only">View notifications</span>
+                        <span className="sr-only"></span>
                         <ShoppingCartIcon className="h-6 w-6" aria-hidden="true" />
                       </button>
+                      <span className="inline-flex items-center rounded-md mb-7 mr-5 bg-red-50 px-2 py-1 text-xs font-medium text-red-700 
+                      ring-1 ring-inset ring-red-600/10">
+                       3
+                           </span>
+
 
                       {/* Profile dropdown */}
                       <Menu as="div" className="relative ml-3">
@@ -122,6 +125,7 @@ function Navbar({children}) {
                         <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
                       )}
                     </Disclosure.Button>
+                    
                   </div>
                 </div>
               </div>
@@ -159,8 +163,12 @@ function Navbar({children}) {
                       <span className="absolute -inset-1.5" />
                       <span className="sr-only">View notifications</span>
                       <ShoppingCartIcon className="h-6 w-6" aria-hidden="true" />
+                      
                     </button>
-                  </div>
+                    <span className="inline-flex items-center rounded-md mb-7 -ml-3 bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
+                             3
+                               </span>
+                   </div>
                   <div className="mt-3 space-y-1 px-2">
                     {userNavigation.map((item) => (
                       <Disclosure.Button
@@ -181,7 +189,7 @@ function Navbar({children}) {
 
         <header className="bg-white shadow">
           <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+            <h1 className="text-3xl font-bold tracking-tight text-gray-900">E-commerce</h1>
           </div>
         </header>
         <main>
