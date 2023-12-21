a31e95fdf7bbf642335c019b9dfde400d7a09d16
unknown <104721799+puja-debnath@users.noreply.github.com>
2023-12-21 18:05:05 +0530

    .....

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-    <Counter ></Counter>
+       <Home></Home>
+     
     </div>
   );
 }
