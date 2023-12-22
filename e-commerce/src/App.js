51412260d103ca48d6924ad829f806c4888a52d5
unknown <104721799+puja-debnath@users.noreply.github.com>
2023-12-22 23:53:35 +0530

    checkout page

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -5,6 +5,7 @@ import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import CartPage from './pages/CartPage';
+import CheckoutPage from './pages/CheckoutPage';
 import {
   createBrowserRouter,
   RouterProvider,
@@ -16,6 +17,7 @@ const router = createBrowserRouter([
   {path: "/signup",element: (<SignupPage></SignupPage>) },
   { path: "/login", element: (<LoginPage></LoginPage>)},
   { path: "/cart", element: (<CartPage></CartPage>)},
+  { path: "/checkout", element: (<CheckoutPage></CheckoutPage>)},
 ]);
 
 function App() {
