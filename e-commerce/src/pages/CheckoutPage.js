d638452ef619900d1989d4d6accf61c0f13ac0ff
unknown <104721799+puja-debnath@users.noreply.github.com>
2023-12-23 01:18:29 +0530

    checkout update

--- e-commerce/src/pages/CheckoutPage.js
+++ e-commerce/src/pages/CheckoutPage.js
@@ -36,18 +36,33 @@ const products = [
  
 ]
 
-const addresses = 
-}
+const addresses = [
+    {
+        name:"puja",
+        street:"kdjf",
+        city:"delhi",
+        pincode:39845,
+        phone:9578378324
+    },
+    {
+        name:"debnath",
+        street:"kdjf",
+        city:"agartal",
+        pincode:39845,
+        phone:9578378324
+    }
+]
+
 
 function CheckoutPage() {
   return (
-    <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-    <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-5">
-        <div className='lg:col-span-3'>
+    <div className="mx-auto max-w-7xl px-0  pt-8 sm:px-0 lg:px-0 ">
+    <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-5 ">
+        <div className='lg:col-span-3  '>
         <form className='bg-white pl-4'>
         <div className="border-b border-gray-900/10 pb-12">
-          <h2 className="text-base font-semibold leading-7 text-gray-900">Personal Information</h2>
-          <p className="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p>
+          <h2 className=" text-2xl font-semibold leading-7 text-gray-900">Personal Information</h2>
+          <p className=" text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p>
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-3">
@@ -175,39 +190,51 @@ function CheckoutPage() {
           </div>
         </div>
 
+
+        
+      <div className="mt-6 flex items-center justify-end gap-x-6">
+        <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
+          Reset
+        </button>
+        <button
+          type="submit"
+          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        >
+          Add address
+        </button>
+      </div>
+
         <div className="border-b border-gray-900/10 pb-12">
           <h2 className="text-base font-semibold leading-7 text-gray-900">Address</h2>
           <p className="mt-1 text-sm leading-6 text-gray-600">
           choose from existing address
           </p>
-          <ul role="list" className="divide-y divide-gray-100">
+          <div className=''>
+          <ul role="list" >
       {addresses.map((address) => (
-        <li key={addresses.email} className="flex justify-between gap-x-6 py-5">
-          <div className="flex min-w-0 gap-x-4">
-           
-            <div className="min-w-0 flex-auto">
-              <p className="text-sm font-semibold leading-6 text-gray-900">{addresses.name}</p>
-              <p className="mt-1 truncate text-xs leading-5 text-gray-500">{addresses.email}</p>
+        <li key={address.name} className="flex justify-between gap-x-6 py-5  border-2 border-gray-700 p-4 mr-4 ">
+          <div className="flex min-w-0 gap-x-4  ">
+          <input
+                   
+                    name="address"
+                    type="radio"
+                    className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                  />
+            <div className="min-w-0 flex-auto border-solid ">
+              <p className="text-sm font-semibold leading-6 text-gray-900">{address.name}</p>
+              <p className="mt-1 truncate text-xs leading-5 text-gray-500">{address.street}</p>
+              <p className="mt-1 truncate text-xs leading-5 text-gray-500">{address.pincode}</p>
+
             </div>
           </div>
-          <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-            <p className="text-sm leading-6 text-gray-900">{person.role}</p>
-            {person.lastSeen ? (
-              <p className="mt-1 text-xs leading-5 text-gray-500">
-                Last seen <time dateTime={person.lastSeenDateTime}>{person.lastSeen}</time>
-              </p>
-            ) : (
-              <div className="mt-1 flex items-center gap-x-1.5">
-                <div className="flex-none rounded-full bg-emerald-500/20 p-1">
-                  <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
-                </div>
-                <p className="text-xs leading-5 text-gray-500">Online</p>
-              </div>
-            )}
-          </div>
+          <div className="hidden shrink-0 sm:flex sm:flex-col sm:items-end mr-6">
+            <p className="text-sm leading-6 text-gray-900">{address.phone}</p>
+            <p className="text-sm leading-6 text-gray-900">{address.city}</p>
+            </div>
         </li>
       ))}
     </ul>
+    </div>
 
 
           <div className="mt-10 space-y-10">
@@ -245,21 +272,10 @@ function CheckoutPage() {
         </div>
       
 
-      <div className="mt-6 flex items-center justify-end gap-x-6">
-        <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
-          Cancel
-        </button>
-        <button
-          type="submit"
-          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-        >
-          Save
-        </button>
-      </div>
         </form>
         </div>
         <div className='lg:col-span-2'>
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 bg-white">
+        <div className="mx-auto max-w-7xl px-0 sm:px-0 lg:px-0 bg-white">
         <h2 className='text-4xl p-4 font-bold'>Cart</h2>
     <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
                       <div className="flow-root">
@@ -321,10 +337,10 @@ function CheckoutPage() {
                     </div>
                     <p className="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
                     <div className="mt-6">
-                    <Link to="/checkout"
+                    <Link to="/pay"
                         className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
                       >
-                        Checkout
+                       Pay and order
                         </Link>
                     </div>
                     <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
